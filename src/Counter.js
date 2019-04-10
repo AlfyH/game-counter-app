778108809bc40c860661ab11758a59de22242f95
Alfy Hushairi <alfyhushairi@gmail.com>
2019-04-10 23:51:40 +0800

    Update Counter.js
    Converted Counter into a class component and added state

--- src/Counter.js
+++ src/Counter.js
@@ -1,16 +1,22 @@
 import React, { Component } from 'react';
 
-const Counter = (props) => {
+class Counter extends Component {
 
+constructor(){
+  super()
+  this.state = {
+    score: 0
+  };
+}
+  render() {
     return (
       <div className = "counter">
         <button className="counter-action decrement"> - </button>
-        <span className="counter-score">{props.score}</span>
+        <span className="counter-score">{this.props.score}</span>
         <button className="counter-action increment"> + </button>
       </div>
     );
   }
-
-
+}
 
 export default Counter;
